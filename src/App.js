25db82f759e0975f412b1ccafce6b0c9569640d4
eduardowanderleyde <wanderley.eduardo@gmail.com>
2022-09-20 22:29:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React from "react";
+import Radio from "./Form/Radio";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const perguntas = [
+  {
+    pergunta: "Qual o maior planeta do sistema solar?",
+    options: ["Marte", "Júpiter", "Saturno"],
+    resposta: "Júpiter",
+    id: "p1",
+  },
+  {
+    pergunta: "Quantos ossos temos no nosso corpo?",
+    options: ["156", "206", "302"],
+    resposta: "206",
+    id: "p2",
+  },
+  {
+    pergunta: "Qual o planeta mais próximo do Sol?",
+    options: ["Netuno", "Júpiter", "Mercúrio"],
+    resposta: "Mercúrio",
+    id: "p3",
+  },
+  {
+    pergunta: "Quantos continentes existem?",
+    options: ["6", "8", "4"],
+    resposta: "6",
+    id: "p4",
+  },
+];
+
+const App = () => {
+  const [respostas, setRespostas] = React.useState({
+    p1: "",
+    p2: "",
+    p3: "",
+    p4: "",
+  });
+  const [slide, setSlide] = React.useState(0);
+  const [resultado, setResultado] = React.useState(null);
+
+  function handleChange({ target }) {
+    setRespostas({ ...respostas, [target.id]: target.value });
+  }
+
+  function resultadoFinal() {
+    const corretas = perguntas.filter(
+      ({ id, resposta }) => respostas[id] === resposta
+    );
+    setResultado(`Você acertou: ${corretas.length} de ${perguntas.length}`);
+  }
+
+  function handleClick() {
+    if (slide < perguntas.length - 1) {
+      setSlide(slide + 1);
+    } else {
+      setSlide(slide + 1);
+      resultadoFinal();
+    }
+  }
+
+  return (
+    <form onSubmit={(event) => event.preventDefault()}>
+      {perguntas.map((pergunta, index) => (
+        <Radio
+          active={slide === index}
+          key={pergunta.id}
+          value={respostas[pergunta.id]}
+          onChange={handleChange}
+          {...pergunta}
+        />
+      ))}
+      {resultado ? (
+        <p>{resultado}</p>
+      ) : (
+        <button
+          type="button"
+          class="btn btn-primary btn-lg"
+          onClick={handleClick}
+          style={{ marginLeft: "1rem" }}
+        >
+          Próxima
+        </button>
+      )}
+    </form>
+  );
+};
+
+export default App;
---
+++ src/Form/Radio.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Radio = ({ pergunta, options, onChange, value, id, active }) => {
+  if (active === false) return null;
+  return (
+    <fieldset
+      style={{
+        padding: "2rem",
+        marginBottom: "1rem",
+        border: "2px solid #eee",
+      }}
+    >
+      <legend
+        className="p-3 mb-2 bg-warning text-dark"
+        style={{ fontWeight: "bold" }}
+      >
+        {pergunta}
+      </legend>
+      {options.map((option) => (
+        <label
+          key={option}
+          style={{
+            marginBottom: "1rem",
+            fontFamily: "monospace",
+            marginLeft: "0.75rem",
+          }}
+        >
+          <input
+            type="radio"
+            id={id}
+            checked={value === option}
+            value={option}
+            onChange={onChange}
+          />
+          {option}
+        </label>
+      ))}
+    </fieldset>
+  );
+};
+
+export default Radio;
